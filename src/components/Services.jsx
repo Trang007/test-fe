534d6e16a51f57339ffa316154b63347cec7b6b1
Trang007 <dangthuytrang007@gmail.com>
2023-02-17 00:49:34 +0700

    test8

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,122 @@
-import React from 'react'
-import style from './Services.module.css'
+import React from "react";
+import style from "./Services.module.css";
+import icon1 from "../assets/service-icon1.png";
 const Services = () => {
   return (
-    <div className={style['Services']}>
-        <div className={style['Services_left']}></div>
-
+    <div className={style["Services"]}>
+      <div className={style["Container"]}>
+        <div className={style["Ser_inner"]}></div>
+        <div className={style["Services_left"]}>
+          <div className={style["Services_left_img"]}>
+            <div className={style["Service_text"]}>
+              <h5>What We Do</h5>
+              <h2>
+                Services we
+                <br />
+                Provide
+              </h2>
+              <p>
+                Quis autem vel eum iure repren
+                <br />
+                derit qui in ea voluptate velit as
+                <br />
+                se ruam nihil molestiae....
+              </p>
+            </div>
+            <div className={style["text"]}>
+              <a href="">Get Started</a>
+            </div>
+          </div>
+        </div>
+        <div className={style["Services_right"]}>
+          <div className={style["group"]}>
+            <a href="" className={style["element4"]}>
+              <div className={style["icon4"]}>
+                <img src={icon1} alt="" />
+              </div>
+              <div className={style["content4"]}>
+                <h1>
+                  Business
+                  <br />
+                  Growth
+                </h1>
+                <h3>
+                  Nostrud exercitation alia
+                  <br />
+                  consequatur auor...
+                </h3>
+                <div className={style["arrow4"]}>
+                  <a href="">a</a>
+                </div>
+              </div>
+            </a>
+            <div className={style["element4"]}>
+              <div className={style["icon4"]}>
+                <img src={icon1} alt="" />
+              </div>
+              <div className={style["content4"]}>
+                <h1>
+                  Business
+                  <br />
+                  Growth
+                </h1>
+                <h3>
+                  Nostrud exercitation alia
+                  <br />
+                  consequatur auor...
+                </h3>
+                <div className={style["arrow4"]}>
+                  <a href="">a</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={style["group"]}>
+            <div className={style["element4"]}>
+              <div className={style["icon4"]}>
+                <img src={icon1} alt="" />
+              </div>
+              <div className={style["content4"]}>
+                <h1>
+                  Business
+                  <br />
+                  Growth
+                </h1>
+                <h3>
+                  Nostrud exercitation alia
+                  <br />
+                  consequatur auor...
+                </h3>
+                <div className={style["arrow4"]}>
+                  <a href="">a</a>
+                </div>
+              </div>
+            </div>
+            <div className={style["element4"]}>
+              <div className={style["icon4"]}>
+                <img src={icon1} alt="" />
+              </div>
+              <div className={style["content4"]}>
+                <h1>
+                  Business
+                  <br />
+                  Growth
+                </h1>
+                <h3>
+                  Nostrud exercitation alia
+                  <br />
+                  consequatur auor...
+                </h3>
+                <div className={style["arrow4"]}>
+                  <a href="">a</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
